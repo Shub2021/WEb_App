f36aa24bcf174c316149b8212f31333447f1b3f3
sajithprasanga <sajithprasanga96@gmail.com>
2021-09-14 13:36:57 +0530

    commit-others

---
+++ constants/icons.js
@@ -0,0 +1,27 @@
+const add = require("../assets/icons/add.png");
+const bookmarkFilled = require("../assets/icons/bookmark-fill.png");
+const bookmark = require("../assets/icons/bookmark.png");
+const heart = require("../assets/icons/heart.png");
+const home = require("../assets/icons/home.png");
+const leftArrow = require("../assets/icons/left-arrow.png");
+const minus = require("../assets/icons/minus.png");
+const night = require("../assets/icons/night.png");
+const rightArrow = require("../assets/icons/right-arrow.png");
+const profile = require("../assets/icons/profile.png");
+const sunny = require("../assets/icons/sunny.png");
+const search = require("../assets/icons/search.png");
+
+export default {
+  add,
+  bookmark,
+  bookmarkFilled,
+  heart,
+  home,
+  leftArrow,
+  minus,
+  night,
+  rightArrow,
+  profile,
+  sunny,
+  search,
+};
---
+++ constants/index.js
@@ -0,0 +1,21 @@
+import icons from "./icons";
+
+import theme, {
+  COLORS,
+  SIZES,
+  FONTS,
+  darkTheme,
+  lightTheme,
+  selectedTheme,
+} from "./theme";
+
+export {
+  icons,
+  theme,
+  COLORS,
+  SIZES,
+  FONTS,
+  darkTheme,
+  lightTheme,
+  selectedTheme,
+};
